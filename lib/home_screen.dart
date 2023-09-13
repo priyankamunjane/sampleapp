e1cbd1d23db68f9748e0f00ecc7e24774bd194e7
priyankaBelure <priyankamunjane@gmail.com>
2023-09-13 16:43:12 +0530

    counter data pushed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,11 +8,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter App'),
+      ),
+      body: Center(
+        child: Text('Button tapped $counter times'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
